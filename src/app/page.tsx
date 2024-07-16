4ebbee65563c244fd32f9c1dd46aaae36eb31380
Sagar2102 <sagar20032610@gmail.com>
2024-07-17 00:49:19 +0530

    Building Featured Card Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
 import Image from "next/image";
 
@@ -5,6 +6,7 @@ export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
     <HeroSection />
+    <FeaturedCourses />
     </main>
   );
 }
