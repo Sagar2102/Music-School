2ed046516b4febd0e6e27f734d6abc35eef4f161
Sagar2102 <sagar20032610@gmail.com>
2024-07-17 01:44:06 +0530

    Testimonial Card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import MusicSchoolTestimonials from "@/components/TestimonialCards";
 import WhyChooseUs from "@/components/WhyChooseUs";
 import Image from "next/image";
 
@@ -9,6 +10,7 @@ export default function Home() {
     <HeroSection />
     <FeaturedCourses />
     <WhyChooseUs />
+    <MusicSchoolTestimonials />
     </main>
   );
 }
