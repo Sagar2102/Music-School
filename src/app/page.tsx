f173d735d42ee4cab8f1eee10a5449f5f33f690c
Sagar2102 <sagar20032610@gmail.com>
2024-07-17 01:53:55 +0530

    Upcoming Webinars

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
 import MusicSchoolTestimonials from "@/components/TestimonialCards";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
 import WhyChooseUs from "@/components/WhyChooseUs";
 import Image from "next/image";
 
@@ -11,6 +12,7 @@ export default function Home() {
     <FeaturedCourses />
     <WhyChooseUs />
     <MusicSchoolTestimonials />
+    <UpcomingWebinars />
     </main>
   );
 }
