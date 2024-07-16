86a2dfa1472608e2d6bc837029949b925863b578
Sagar2102 <sagar20032610@gmail.com>
2024-07-17 01:17:49 +0530

    Sticky Scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import WhyChooseUs from "@/components/WhyChooseUs";
 import Image from "next/image";
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
     <HeroSection />
     <FeaturedCourses />
+    <WhyChooseUs />
     </main>
   );
 }
