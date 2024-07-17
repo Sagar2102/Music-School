b66d4663605ce7a5c65bc7037fd9f82252134937
Sagar2102 <sagar20032610@gmail.com>
2024-07-17 13:08:37 +0530

    Footer & Contact us Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
+import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
 import MusicSchoolTestimonials from "@/components/TestimonialCards";
 import UpcomingWebinars from "@/components/UpcomingWebinars";
 import WhyChooseUs from "@/components/WhyChooseUs";
@@ -13,6 +15,8 @@ export default function Home() {
     <WhyChooseUs />
     <MusicSchoolTestimonials />
     <UpcomingWebinars />
+    <Instructors />
+    <Footer />
     </main>
   );
 }
